d7b4e8a0703f012e07f8003551fd5313d803a415
adityakusuma23 <111201508735@mhs.dinus.ac.id>
2017-10-22 23:35:31 +0700

    versi 1.0

--- app/src/main/java/net/aditkus/newasyncinternet/MainActivity.java
+++ app/src/main/java/net/aditkus/newasyncinternet/MainActivity.java
@@ -6,15 +6,25 @@ import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ConnectInternetTask c1;
     static TextView myText;
     static EditText myEdit;
+    private Spinner mySpin;
+    String isi;
+    private String[] proto = {
+            "http://",
+            "https://" };
 
 
     @Override
@@ -23,9 +33,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myText = (TextView) findViewById(R.id.myResult);
         myEdit = (EditText) findViewById(R.id.dataMasuk);
+        mySpin = (Spinner) findViewById(R.id.spinner);
+
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, proto);
+        mySpin.setAdapter(adapter);
+
+
+        mySpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                isi =mySpin.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+
+        });
 
 
     }
+
+
+
 
     public void doSomething(View view) {
         ConnectivityManager cm = (ConnectivityManager) getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -34,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
         {
             c1 = new ConnectInternetTask(this);
             String input = myEdit.getText().toString();
-            c1.execute(input);
+            String all = isi.concat(input);
+            c1.execute(all);
         }
         else
         {
